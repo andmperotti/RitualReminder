04643b10c5050e223236b9a651e263f0ab19c0f1
Perotti28 <51537156+Perotti28@users.noreply.github.com>
2021-12-06 09:52:22 -0800

    Update LightweightRitualReminder.py

--- LightweightRitualReminder.py
+++ LightweightRitualReminder.py
@@ -6,14 +6,14 @@ import datetime
 clienttxt = 'C:/Program Files (x86)/Grinding Gear Games/Path of Exile/logs/Client.txt' #path to your Client.txt file
 
 def popup():
-    """Pops up a message to the user when they interact with a Ritual to remind them to spend their tribute."""
+    """Pops up a message to the user (which persists on top of the games window) when they interact with a Ritual to remind them to spend their tribute."""
     objectFinder = tk.Tk()
-    objectFinder.wm_attributes("-topmost", 1) #Persists over other windows
+    objectFinder.wm_attributes("-topmost", 1) 
     logFrame = tk.LabelFrame(objectFinder, text="Ritual Reminder")
     logFrame.pack(fill="both", expand=True)
     menu_Label = ttk.Label(logFrame, text="SPEND YOUR TRIBUTE!")
     menu_Label.pack(expand=True)
-    objectFinder.geometry(f"200x100+920+750") #the second 2 numbers are the postioning (X+Y) of pixels of the top left of the popup.
+    objectFinder.geometry(f"200x100+920+750") #the second 2 numbers are the postioning (X+Y) of pixels of the top left corner of the popup window.
     objectFinder.mainloop()
 
 with open(clienttxt, encoding='utf-8') as f:
@@ -21,8 +21,8 @@ with open(clienttxt, encoding='utf-8') as f:
     while True:
         line = f.readline()
         if "FindClosestObject" in line:
-            print(f"Ritual Interacted with at:{datetime.datetime.now()}") #This prints out a record of rituals you clicked on in the terminal that will popup and run the script.
+            print(f"Ritual Interacted with at:{datetime.datetime.now()}") #This prints out a record of rituals you clicked on in the terminal (command prompt) that will popup and run the script.
             popup()
         if not line:
-            time.sleep(1) #This is the delay, 1 second if it annoys you, then you can set it to 0.5 for half a second, etc.
+            time.sleep(1) #This is the delay, (1 second). You can pass in .1 for tenth of a second, .5 half second, 2 for 2 seconds, etc.
             continue
